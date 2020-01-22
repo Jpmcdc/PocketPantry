60eb7fff2263baedde3e93746899f3950118ba21
masonkn <masonandmckay@gmail.com>
2020-01-22 14:58:49 -0700

    Pocket Pantry Tabbed Sample Template Start

---
+++ Pocket_Pantry/Pocket_Pantry/Pocket_Pantry/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Pocket_Pantry.Models;
+
+namespace Pocket_Pantry.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
---
+++ Pocket_Pantry/Pocket_Pantry/Pocket_Pantry/Views/AboutPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Pocket_Pantry.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
