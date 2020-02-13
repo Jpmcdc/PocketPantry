89b1e9dc9a76d51a22dea0830d9cec059d474d13
jpmcdc <jpmcdc@gmail.com>
2020-02-13 10:08:56 -0700

    Worked on Recipie View
    - Added new class (View_Recipe)
    - Changed Class folder name to Models

---
+++ Pocket_Pantry/Pocket_Pantry/Pocket_Pantry/View_Recipe.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Pocket_Pantry {
+    public partial class View_Recipe : ContentPage {
+
+        public View_Recipe() {
+            InitializeComponent();
+        }
+    }
+}
