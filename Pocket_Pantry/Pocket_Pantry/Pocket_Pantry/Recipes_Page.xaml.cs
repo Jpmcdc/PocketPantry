4005d6ee7557a81e5eda1a8d7dc8656a3d889a4b
jpmcdc <jpmcdc@gmail.com>
2020-02-19 15:30:29 -0700

    Recipe List Fix
    - Added ViewModels Folder
    - Added RecipeViewModel
    - Fixed Bug where Recipe List would not populate

--- Pocket_Pantry/Pocket_Pantry/Pocket_Pantry/Recipes_Page.xaml.cs
+++ Pocket_Pantry/Pocket_Pantry/Pocket_Pantry/Recipes_Page.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Pocket_Pantry.ViewModels;
 
 using Xamarin.Forms;
 
@@ -11,63 +12,26 @@ namespace Pocket_Pantry {
          *  This list will populate the Recipies_List_View in the xaml
          */
         public IList<Recipe> Recipes_List { get; private set; }
-    
-    
-        public Recipes_Page() {
+
+
+        public Recipes_Page()
+        {
             InitializeComponent();
-
-            /**
-             *  Extanziate Recipies_List and populate
-             *  TODO: Figue out if this is where all the information will be stored
-             */
-            Recipes_List = new List<Recipe> {
-
-                new Recipe {
-                    title = "Greek Salad",
-                    type = "Salads"
-
-                },
-
-                new Recipe {
-                    title = "Onion Soup",
-                    type = "Soups"
-
-                },
-
-                 new Recipe {
-                    title = "Chicken Enchiladas",
-                    type = "Mexican"
-
-                },
-
-                  new Recipe {
-                    title = "Lemon Meringue",
-                    type = "Cheesecake"
-
-                },
-
-                   new Recipe {
-                    title = "Grilled Peanut Butter and Banana Sandwich",
-                    type = "Sandwich"
-
-                }
-            };
-
-            BindingContext = this;
+            BindingContext = new RecipeViewModel();
         }
 
         
-        private void ListView_ItemSelected(object sender, SelectedItemChangedEventArgs e) {
-            Recipe selectedItem = e.SelectedItem as Recipe;
-        }
+        //private void ListView_ItemSelected(object sender, SelectedItemChangedEventArgs e) {
+        //    Recipe selectedItem = e.SelectedItem as Recipe;
+        //}
 
         /**
          *  Handle the event of clicking on a recipie in the list
          *  This should open a Recipe Page with all the information form the specific recipe
          */
-        private void ListView_ItemTapped(object sender, ItemTappedEventArgs e) {
-            Recipe tappedItem = e.Item as Recipe;
-        }
+        //private void ListView_ItemTapped(object sender, ItemTappedEventArgs e) {
+        //    Recipe tappedItem = e.Item as Recipe;
+        //}
 
       
     }
---
+++ Pocket_Pantry/Pocket_Pantry/Pocket_Pantry/ViewModels/RecipeViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace Pocket_Pantry.ViewModels
+{
+    public class RecipeViewModel
+    {
+
+        public ObservableCollection<Recipe> Recipe_List { get; set; }
+
+        public RecipeViewModel()
+        {
+            Recipe GreekSalad = new Recipe
+            {
+                title = "Greek Salad",
+                type = "Salads"
+            };
+
+            Recipe OnionSoup = new Recipe
+            {
+                title = "Onion Soup",
+                type = "Soups"
+            };
+
+            Recipe Enchiladas = new Recipe
+            {
+                title = "Chicken Enchiladas",
+                type = "Mexican"
+            };
+
+            Recipe LemonMeringue = new Recipe
+            {
+                title = "Lemon Meringue",
+                type = "Cheesecake"
+            };
+
+            Recipe PBSandwitch = new Recipe
+            {
+                title = "Grilled Peanut Butter and Banana Sandwich",
+                type = "Sandwich"
+            };
+
+            Recipe_List = new ObservableCollection<Recipe>();
+            Recipe_List.Add(GreekSalad);
+            Recipe_List.Add(OnionSoup);
+            Recipe_List.Add(Enchiladas);
+            Recipe_List.Add(LemonMeringue);
+            Recipe_List.Add(PBSandwitch);
+        }
+    }
+}
