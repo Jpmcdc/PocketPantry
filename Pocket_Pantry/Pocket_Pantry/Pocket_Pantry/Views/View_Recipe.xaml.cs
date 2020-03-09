5e6b394c50b6ad61e6885622fff31690e693bd78
jpmcdc <jpmcdc@gmail.com>
2020-03-09 15:10:40 -0600

    Added Scan Btns
    - Also added more comments

--- Pocket_Pantry/Pocket_Pantry/Pocket_Pantry/Views/View_Recipe.xaml.cs
+++ Pocket_Pantry/Pocket_Pantry/Pocket_Pantry/Views/View_Recipe.xaml.cs
@@ -7,12 +7,19 @@ using Xamarin.Forms;
 namespace Pocket_Pantry {
     public partial class View_Recipe : ContentPage {
 
+        /**
+         *  View_Recipe Constructor
+         */
         public View_Recipe(String title, String type) {
             InitializeComponent();
 
             View_Title.Text = title;
         }
 
+        /**
+         *  Handles "Back" button
+         *  Navigates to the previous page
+         */
         async void View_Recipe_Back(System.Object sender, System.EventArgs e)
         {
             await Navigation.PopModalAsync();
