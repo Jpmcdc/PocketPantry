cca640196d77ef63892ffa80ef55ebca7dd139d1
MarcusBacke <59940197+MarcusBacke@users.noreply.github.com>
2020-03-14 19:25:24 -0600

    new ingredient page done
    still working on data_base

--- Pocket_Pantry/Pocket_Pantry/Pocket_Pantry/Views/New_Ingredient.xaml.cs
+++ Pocket_Pantry/Pocket_Pantry/Pocket_Pantry/Views/New_Ingredient.xaml.cs
@@ -2,6 +2,14 @@
 using System.Collections.Generic;
 
 using Xamarin.Forms;
+using SQLite;
+using Xamarin.Forms.Xaml;
+using Plugin.Media.Abstractions;
+using Plugin.Media;
+using System.IO;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Linq;
 
 namespace Pocket_Pantry.Views
 {
@@ -11,5 +19,39 @@ namespace Pocket_Pantry.Views
         {
             InitializeComponent();
         }
+
+
+
+        async void cancel_btn(System.Object sender, System.EventArgs e)
+        {
+            await Navigation.PopModalAsync();
+        }
+
+        async void Save_Clicked(System.Object sender, System.EventArgs e)
+        {
+            // add the new recipe using Recipe
+            Recipe newRecipe = new Recipe();
+            {
+                //newRecipe.title = Entry_Title.Text;
+                //newRecipe.ingredients = Entry_Ingredients.Text;
+                //newRecipe.directions = Entry_Directions.Text;
+            };
+
+            //connection to the datbase & saving information
+            SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation);
+            conn.CreateTable<Recipe>();
+            int rows = conn.Insert(newRecipe);
+            conn.Close();
+
+            if (rows > 0)
+            {
+                //_ = DisplayAlert("Success: ", "Recipe succesfully inserter", "OK");
+                await Navigation.PopModalAsync();
+            }
+            else
+                //_ = DisplayAlert("Failure: ", "Recipe failed to be inserter", "OK");
+                await Navigation.PopModalAsync();
+        }
+
     }
 }
